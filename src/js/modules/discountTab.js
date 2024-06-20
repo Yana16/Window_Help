6e1cf3226213835577ddae68cfee39f5a312d389
Yana <example@kr.net>
2024-06-20 11:31:25 +0300

    24

--- src/js/modules/discountTab.js
+++ src/js/modules/discountTab.js
@@ -1,11 +1,11 @@
-const discountMark = () => {
-  let discountTable = document.getElementsByClassName("discount-widjet")[0];
+// const discountMark = () => {
+//   let discountTable = document.getElementsByClassName("discount-widjet")[0];
 
-  discountTable.addEventListener("click", (e) => {
-    console.log("CLICK");
-    if (e.target) {
-      discountTable.style.display = "none";
-    }
-  });
-};
-export default discountMark;
+//   discountTable.addEventListener("click", (e) => {
+//     console.log("CLICK");
+//     if (e.target) {
+//       discountTable.style.display = "none";
+//     }
+//   });
+// };
+// export default discountMark;
